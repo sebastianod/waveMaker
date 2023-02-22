8402ef4610e284ed7c600737096e59e229387186
sebastian <sebastianochoad@gmail.com>
2023-02-22 18:11:57 -0500

    Added CORS to flask backend in order for React frontend to retrieve data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState ,useRef } from "react";
 
-function App() {
+function App() { 
+  
+  //------------Receiving------------//
+  const [data, setData] = useState({
+    name: "",
+    age: 0,
+    date: "",
+    programming: "",
+  });
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/data") //continuing the proxy http://localhost:5000/ in package.json
+    .then(
+      (res)=>{res.json()
+        .then((data)=>{
+          const { Name, Age, Date, Programming} = data; //destructuring python data
+          setData({
+            name: Name, //exactly as the property is named in the python app
+            age: Age,
+            date: Date,
+            programming: Programming,
+          });
+        });
+      })
+  }, []);
+
+  const { name, age, date, programming } = data; //destructuring our own react data
+
   return (
-    <div>
-      <h1> Frontend UI goes here! </h1>
+    <div className="App">
+      <header className="App-header">
+        <h1> React retrieving flask data </h1>
+        <p>{name}</p>
+        <p>{age}</p>
+        <p>{date}</p>
+        <p>{programming}</p>
+      </header>
     </div>
   );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,25 +1,26 @@
 # Import flask and datetime module for showing date and time
 from flask import Flask
+from flask_cors import CORS
 import datetime
-
-x = datetime.datetime.now()
 
 # Initializing flask app
 app = Flask(__name__)
 
+#enable CORS
+CORS(app)
+
 # Route for seeing a data
 @app.route('/data')
-def get_time():
+def get_data():
+    x = datetime.datetime.now()
+    # Returning an api for showing in reactjs
+    return {
+        'Name': 'geek', 
+        'Age': '22',
+        'Date': x.strftime('%Y-%m-%d %H:%M:%S'), 
+        'Programming': 'python'
+    }
 
-	# Returning an api for showing in reactjs
-	return {
-		'Name':"geek",
-		"Age":"22",
-		"Date":x,
-		"programming":"python"
-		}
-
-	
 # Running app
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
