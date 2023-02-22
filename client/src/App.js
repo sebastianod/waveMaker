82d592b9beb00863040279e088b544a1d6b7b27a
sebastian <sebastianochoad@gmail.com>
2023-02-21 19:45:51 -0500

    1. Made waves with SVG output 2. Set up flask server 3. Set up React and deleted basic files

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React, { useEffect, useRef } from "react";
+
+function App() {
+  return (
+    <div>
+      <h1> Frontend UI goes here! </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ flask-server/waveGen.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Domain
+x = np.linspace(0, 2*np.pi, 1000)
+
+#Fourier Series
+def fourierSeries(height, mainAmplitude, domain, crazyness): #the higher n, the quicker you see a pattern
+  y = height
+  for i in range(crazyness):
+    an = np.random.rand()
+    bn = np.random.rand()
+    y += an*np.cos(i*domain) + bn*np.sin(i*domain)
+  return mainAmplitude*y
+
+y = fourierSeries(0, 0.5, x, 6)
+
+#Above zero and height
+actualY = y - y.min() + 14 #take the minimum negative value and add a height
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Plot the sine wave
+#ax.plot(x, actualY, color='blue')
+
+# Fill the area underneath the curve with color
+ax.fill_between(x, 0, actualY, where=actualY>0, interpolate=True, color='#3D2386', alpha=1)
+
+#Fill the area above the curve with red
+#ax.fill_between(x, actualY.max()+5, actualY, where=actualY>0, interpolate=True, color='red', alpha=1)
+
+# set x and y limits of the plot
+ax.set_ylim(0.0,actualY.max()+5)# maximum value of the function plus a constant
+ax.set_xlim(0.0, x[-1]) #from zero to the last value of the x array
+
+#Do not show axis
+ax.axis('off')
+
+# Set the background color of the fifure
+fig.set_facecolor('#6A65AB')
+
+# Set the padding around the plot to zero
+fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
+
+#save the plot, must include the color of the figure, no edge colors and no padding
+fig.savefig("test.svg", facecolor=fig.get_facecolor(), edgecolor='none', pad_inches=0)
